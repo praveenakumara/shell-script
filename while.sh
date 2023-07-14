ef0554d8af93a832d2306d905bd4c2a97dd4261e
praveenakumara <124884948+praveenakumara@users.noreply.github.com>
2023-07-14 23:56:40 +0530

    Create while.sh

---
+++ while.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#count=0
+#num=10
+#while [ $count -le $num ]
+#do
+#       echo number are $count
+#       let count++
+#       sleep 3
+#done
+num=0
+count=10
+
+while [ $num -le $count ]
+do
+        echo number is $num
+        let num++
+done
