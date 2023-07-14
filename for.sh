aa937dff444dfe1c2229281c40010d99f2d70a6e
praveenakumara <124884948+praveenakumara@users.noreply.github.com>
2023-07-14 23:54:04 +0530

    Create for.sh

---
+++ for.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#for i in 1 2 3 4 5 6
+#do
+#       echo number is $i
+#done
+#for num in {1..10}
+#do
+#       echo "number is $num"
+#done
+for gods in shiv shankara bolenath nilakant ram krishna
+do
+        echo my lovable god is $gods
+done
